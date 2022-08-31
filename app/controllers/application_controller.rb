52b75bc9cef1e6b1329b55434ed7f9e970427896
alan <alan001tk@gmail.com>
2022-08-31 09:28:08 -0700

    Finish phase 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,49 @@
 class ApplicationController < ActionController::API
   before_action :snake_case_params
 
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])# user whose `session_token` == token in `session` cookie
+  end
+
+  def login!(user)
+    # reset `user`'s `session_token` and store in `session` cookie
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout!
+    # reset the `current_user`'s session cookie, if one exists
+    # clear out token from `session` cookie
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil # so that subsequent calls to `current_user` return nil
+  end
+
+  def require_logged_in
+    unless current_user
+      render json: { message: 'Unauthorized' }, status: :unauthorized 
+    end
+  end
+
+  # Testing
+  def test
+    if params.has_key?(:login)
+      login!(User.first)
+    elsif params.has_key?(:logout)
+      logout!
+    end
+
+    if current_user
+      render json: { user: current_user.slice('id', 'username', 'session_token') }
+    else
+      render json: ['No current user']
+    end
+  end
+
   private
 
   def snake_case_params
