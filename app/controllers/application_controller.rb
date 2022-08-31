815827c690887e7dc1efcd5df398be6c8ec7d247
alan <alan001tk@gmail.com>
2022-08-31 11:59:36 -0700

    Finish setup for frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,21 +36,6 @@ class ApplicationController < ActionController::API
     end
   end
 
-  # Testing
-  def test
-    if params.has_key?(:login)
-      login!(User.first)
-    elsif params.has_key?(:logout)
-      logout!
-    end
-
-    if current_user
-      render json: { user: current_user.slice('id', 'username', 'session_token') }
-    else
-      render json: ['No current user']
-    end
-  end
-
   private
 
   def invalid_authenticity_token
--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -9,7 +9,7 @@ const csrfFetch = async (url, options = {}) => {
   if (options.method.toUpperCase() !== 'GET') {
     options.headers['Content-Type'] = 
       options.headers['Content-Type'] || 'application/json';
-    options.headers['X-CSRF-Token'] = sessionStorage.getItem('X-CSRF-Toekn');
+    options.headers['X-CSRF-Token'] = sessionStorage.getItem('X-CSRF-Token');
   }
 
   // call fetch with url and updated options hash
